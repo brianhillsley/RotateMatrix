01f82fb7e7a17fa7289adb953cd2005031a08075
brianhillsley <bhills@mixa.local>
2016-10-18 15:50:48 -0400

    Filled the Runner with minimal code needed 2x2 matrix test

--- RotateMatrix/src/MatrixRotation.java
+++ RotateMatrix/src/MatrixRotation.java
@@ -1,4 +1,11 @@
 
 public class MatrixRotation {
+	int[][] mat;
 	
+	MatrixRotation(int[][] matrix){
+		mat = matrix;
+	}
+	public int[][] rotateMatrix(){
+		// logic for rotating the matrix goes here.
+	}
 }
--- RotateMatrix/src/Runner.java
+++ RotateMatrix/src/Runner.java
@@ -1,4 +1,13 @@
-
+/**
+ * Will use MatrixRotation to rotate a matrix.
+ * @author bhills
+ *
+ */
 public class Runner {
-
+	public static void main(String[] args){
+		int[][] mat = new int[][]{{2,5},{3,9}};
+		int[][] result;
+		MatrixRotation rotateMachine = new MatrixRotation(mat);
+		result = rotateMachine.rotateMatrix();
+	}
 }
